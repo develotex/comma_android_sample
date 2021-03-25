15bdad6f8cbe35906d99ee1ba07db93009bf517f
Anton Shakhov <hakkazuu@gmail.com>
2021-03-25 15:03:38 +0300

    - Removing unused classes

--- comma_sample/src/main/java/io/develotex/comma_sample/ui/home/HomeViewModel.java
+++ comma_sample/src/main/java/io/develotex/comma_sample/ui/home/HomeViewModel.java
@@ -3,10 +3,7 @@ package io.develotex.comma_sample.ui.home;
 import android.app.Application;
 
 import androidx.annotation.NonNull;
-import androidx.lifecycle.MutableLiveData;
 
-import io.develotex.comma_sample.data.DataModel;
-import io.develotex.comma_sample.data.SharedPrefs;
 import io.develotex.comma_sample.ui.BaseViewModel;
 
 
@@ -14,13 +11,6 @@ public class HomeViewModel extends BaseViewModel {
 
     public HomeViewModel(@NonNull Application application) {
         super(application);
-    }
-
-    public MutableLiveData<DataModel> saveCredentials(String userId, String userName) {
-        SharedPrefs.getInstance().setUserId(userId);
-        SharedPrefs.getInstance().setUserName(userName);
-
-        return new MutableLiveData<>(DataModel.success(null));
     }
 
 }
